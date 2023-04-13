3d49311cf60c267b3fbcde8387f4923fceee87b9
Salvo <pellegrino.salvatore02@gmail.com>
2023-04-13 08:55:52 +0200

    Add exercise oop Adv 2

---
+++ oopAdv_2/exercise_oopAdv_2/Movable.java
@@ -0,0 +1,6 @@
+package javaoopadvanced.oopAdv_2.exercise_oopAdv_2;
+
+public interface Movable {
+    String moveForward();
+    String moveBackward();
+}
---
+++ oopAdv_2/exercise_oopAdv_2/Playable.java
@@ -0,0 +1,5 @@
+package javaoopadvanced.oopAdv_2.exercise_oopAdv_2;
+
+public interface Playable {
+    String play();
+}
